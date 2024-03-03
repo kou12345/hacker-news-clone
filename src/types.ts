4711a56baf64fbe5db1e39df90806441a3719c88
kou12345 <hi.0412.ko.2002@gmail.com>
2024-03-04 05:28:58 +0900

    APIのresponseの型を定義

---
+++ src/types.ts
@@ -0,0 +1,23 @@
+// 記事
+type ResStory = {
+    by: string;
+    descendants: number;
+    id: number;
+    kids: number[];
+    score: number;
+    time: number;
+    title: string;
+    type: string;
+    url: string;
+}
+
+type ResComment = {
+    by: string;
+    id: number;
+    kids: number[];
+    parent: number;
+    text: string;
+    time: number;
+    type: string;
+}
+
