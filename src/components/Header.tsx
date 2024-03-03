626366e7374da0dc5e726b3916fbfcb51b730fcd
kou12345 <hi.0412.ko.2002@gmail.com>
2024-03-04 04:49:14 +0900

    add

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import { AppBar, Typography } from "@mui/material";
+import React from "react";
+
+export const Header = () => {
+  return (
+    <AppBar
+      position="static"
+      component="header"
+      sx={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        height: "64px",
+        zIndex: 1,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Typography variant="h6">Hacker News Clone</Typography>
+    </AppBar>
+  );
+};
