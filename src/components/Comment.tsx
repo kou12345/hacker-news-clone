a4cdc31764a5feedd943d7ef307a4519292c8f0e
kou12345 <hi.0412.ko.2002@gmail.com>
2024-03-04 22:25:06 +0900

    コメントを表示するcomponentを追加

---
+++ src/components/Comment.tsx
@@ -0,0 +1,23 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+type Props = {
+  comment: ResComment;
+};
+
+export const Comment = (props: Props) => {
+  const { comment } = props;
+  return (
+    <Box
+      sx={{
+        border: "1px solid #34D399",
+      }}
+    >
+      <Typography variant="body1">{comment.text}</Typography>
+      {/* <Typography variant="body1">{story.url}</Typography> */}
+      <Typography variant="body2">
+        {/* {convertUnixTimeToDate(story.time).toISOString()} */}
+      </Typography>
+    </Box>
+  );
+};
