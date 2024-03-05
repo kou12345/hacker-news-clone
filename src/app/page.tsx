ad46290f2d3b659aad03679f820ebea1730916c1
kou12345 <hi.0412.ko.2002@gmail.com>
2024-03-06 00:27:58 +0900

    閲覧可能な記事を任意数取得する処理を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,57 @@
-import { Comment } from "@/components/Comment";
 import { Story } from "@/components/Story";
-import { Button, Stack, Typography } from "@mui/material";
+import { ResStory, TStory, storySchema } from "@/types";
+import { Box } from "@mui/material";
 
 export default async function Home() {
-  console.log("Hello, world!");
+  console.log("/app/page.tsx");
 
-  const res = await fetch(
-    "https://hacker-news.firebaseio.com/v0/item/8863.json"
-  );
-  const json = await res.json();
-  const story: ResStory = {
-    by: json.by,
-    descendants: json.descendants,
-    id: json.id,
-    kids: json.kids,
-    score: json.score,
-    time: json.time,
-    title: json.title,
-    type: json.type,
-    url: json.url,
+  const fetchStories = async (): Promise<TStory[]> => {
+    // 最大500件の新しいストーリーを取得
+    const res = await fetch(
+      `https://hacker-news.firebaseio.com/v0/newstories.json`
+    );
+
+    if (!res.ok) {
+      console.log("res.ok is false");
+      throw new Error("Network response was not ok.");
+    }
+
+    const storyIds = await res.json();
+    const stories: TStory[] = [];
+    let index = 0;
+
+    // 10件の有効なストーリーを取得するまで、またはIDのリストをすべてチェックするまでループ
+    while (stories.length < 20 && index < storyIds.length) {
+      const id = storyIds[index];
+      const storyRes = await fetch(
+        `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+      );
+
+      if (!storyRes.ok) {
+        index++;
+        continue; // 次のストーリーIDにスキップ
+      }
+
+      const storyData = await storyRes.json();
+      const parsedStory = storySchema.safeParse(storyData);
+
+      if (parsedStory.success) {
+        stories.push(parsedStory.data);
+      }
+
+      index++;
+    }
+
+    return stories;
   };
 
+  const stories = await fetchStories();
+
   return (
-    <div>
-      <Story story={story} />
-
-      {Object.keys(story).map((key) => {
-        return (
-          <div key={key}>
-            <Typography
-              variant="h6"
-              sx={{
-                color: "#77ba9b",
-                // color: "#3b5998",
-              }}
-            >
-              {key}
-            </Typography>
-            <Typography variant="body1">
-              {story[key as keyof typeof story]}
-            </Typography>
-          </div>
-        );
-      })}
-
-      <hr />
-    </div>
+    <Box>
+      {stories.map((story, idx) => (
+        <Story key={idx} story={story} />
+      ))}
+    </Box>
   );
 }
