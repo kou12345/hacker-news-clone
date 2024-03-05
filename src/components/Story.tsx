ec18455061a65728ec9c0eb1d9f9745fcb909e37
kou12345 <hi.0412.ko.2002@gmail.com>
2024-03-05 17:34:15 +0900

    LinkをCustomLinkに変更

--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -1,6 +1,7 @@
 import { convertUnixTimeToDate } from "@/utils/time";
 import { Box, Typography } from "@mui/material";
 import React from "react";
+import { CustomLink } from "./CustomLink";
 
 type Props = {
   story: ResStory;
@@ -10,8 +11,12 @@ export const Story = (props: Props) => {
   const { story } = props;
   return (
     <Box>
-      <Typography variant="h4">{story.title}</Typography>
+      <CustomLink href={`/${story.id}`}>
+        <Typography variant="h4">{story.title}</Typography>
+      </CustomLink>
+
       <Typography variant="body1">{story.url}</Typography>
+
       <Typography variant="body2">
         {convertUnixTimeToDate(story.time).toISOString()}
       </Typography>
