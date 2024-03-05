e2680518523cf820764eb82a5680f4c2b61d266b
kou12345 <hi.0412.ko.2002@gmail.com>
2024-03-05 17:35:12 +0900

    renderCommentsで再帰的にコメントを表示

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,3 +1,5 @@
+import { renderComments } from "@/app/[id]/page";
+import { convertUnixTimeToDate } from "@/utils/time";
 import { Box, Typography } from "@mui/material";
 import React from "react";
 
@@ -8,17 +10,23 @@ type Props = {
 export const Comment = (props: Props) => {
   const { comment } = props;
   return (
-    <Box
-      sx={{
-        // border: "1px solid #34D399",
-        border: "1px solid #77ba9b",
-      }}
-    >
-      <Typography variant="body1">{comment.text}</Typography>
-      {/* <Typography variant="body1">{story.url}</Typography> */}
-      <Typography variant="body2">
-        {/* {convertUnixTimeToDate(story.time).toISOString()} */}
-      </Typography>
-    </Box>
+    <div>
+      <Box
+        sx={{
+          border: "1px solid #77ba9b",
+        }}
+      >
+        <Typography variant="body1">{comment.text}</Typography>
+        <Typography variant="body2">{comment.by}</Typography>
+        <Typography variant="body2">
+          {convertUnixTimeToDate(comment.time).toISOString()}
+        </Typography>
+      </Box>
+      {comment.children && (
+        <div style={{ marginLeft: "20px" }}>
+          {renderComments(comment.children)}
+        </div>
+      )}
+    </div>
   );
 };
