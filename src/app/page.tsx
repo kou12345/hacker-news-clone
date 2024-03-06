8a48f1d2a10b7bfbadce294f6e0260f4cef268ee
kou12345 <hi.0412.ko.2002@gmail.com>
2024-03-07 05:48:36 +0900

    Hacker News APIのURLを修正し、ストーリーの取得方法を変更***

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   const fetchStories = async (): Promise<TStory[]> => {
     // 最大500件の新しいストーリーを取得
     const res = await fetch(
-      `https://hacker-news.firebaseio.com/v0/newstories.json`
+      `https://hacker-news.firebaseio.com/v0/topstories.json`
     );
 
     if (!res.ok) {
@@ -20,32 +20,30 @@ export default async function Home() {
     const stories: TStory[] = [];
     let index = 0;
 
-    // 10件の有効なストーリーを取得するまで、またはIDのリストをすべてチェックするまでループ
-    while (stories.length < 20 && index < storyIds.length) {
-      const id = storyIds[index];
+    for (let i = 0; i < storyIds.length; i++) {
+      console.log("fetch: ", i);
       const storyRes = await fetch(
-        `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+        `https://hacker-news.firebaseio.com/v0/item/${storyIds[i]}.json`
       );
 
       if (!storyRes.ok) {
-        index++;
-        continue; // 次のストーリーIDにスキップ
+        console.error("ERROR: 正しくデータ取得できませんでした");
+        console.error(storyRes.status);
       }
 
       const storyData = await storyRes.json();
       const parsedStory = storySchema.safeParse(storyData);
-
       if (parsedStory.success) {
         stories.push(parsedStory.data);
       }
-
-      index++;
     }
 
     return stories;
   };
 
   const stories = await fetchStories();
+
+  console.log("stories: ", stories.length);
 
   return (
     <Box>
