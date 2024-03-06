9c2cb298968fb8f36b514697d5af6c3c1b851df4
kou12345 <hi.0412.ko.2002@gmail.com>
2024-03-07 05:47:26 +0900

    fix formatDate

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,5 +1,10 @@
-// Unix TimeをDate型に変換する関数
-export const convertUnixTimeToDate = (unixTime: number): Date => {
-  return new Date(unixTime * 1000);
-}
+export const formatDate = (unixTimestamp: number): string => {
+  const date = new Date(unixTimestamp * 1000);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
 
+  return `${year}/${month}/${day} ${hours}:${minutes}`;
+};
